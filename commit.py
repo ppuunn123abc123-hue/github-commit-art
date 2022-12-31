e9ab00bd6f9c1b858ecb87b43425af2dd16eb22b
ppuunn123abc123-hue <ppuunn123abc123@gmail.com>
2023-01-01 00:00:00 +0530

    Adding letter 'B' on 2023-01-01

--- commit.py
+++ commit.py
@@ -5,34 +5,36 @@ from datetime import datetime, timedelta
 # Set up the GitHub repo location
 repo_dir = r"E:\\PUNYA PROJECTS\\commit-art\\github-commit-art"  # raw string avoids escape issues
 os.chdir(repo_dir)
-# Define the commit message art and days between commits
-letters = ['B', 'I', 'T', 'C', 'H', ' ', 'P', 'L', 'Z']  # The phrase
-commit_dates = []
-start_date = datetime(2023, 1, 1)  # Start date for the first commit (Change if needed)
 
-# Create a list of commit dates (one per day)
-for i in range(len(letters)):
-    commit_dates.append(start_date + timedelta(days=i))
+# Define the commit message art and days between commits
+letters = ['B', 'I', 'T', 'C', 'H', ' ', 'P', 'L', 'Z']
+start_date = datetime(2023, 1, 1)
 
 # Function to create and commit each change
 def create_commit(letter, commit_date):
-    # Create a dummy file with the letter
     filename = "commit_art.txt"
-    with open(filename, "w") as file:
-        file.write(letter)  # Write the letter to the file
     
-    # Stage the file and commit it with a specific date
-    subprocess.run(['git', 'add', filename])
-    commit_message = f"Adding letter {letter} to commit art"
-    subprocess.run([
-        'git', 'commit', '--amend', '--no-edit', '--date', commit_date.strftime('%a %b %d %H:%M:%S %Y %z')
-    ])
+    # Write the letter to the file
+    with open(filename, "w") as file:
+        file.write(letter)
+    
+    # Format the commit date (with timezone)
+    date_str = commit_date.strftime('%Y-%m-%dT%H:%M:%S') + '+0530'  # IST timezone
+    
+    env = os.environ.copy()
+    env["GIT_AUTHOR_DATE"] = date_str
+    env["GIT_COMMITTER_DATE"] = date_str
 
-    # Push the commit (this assumes you have a remote setup)
-    subprocess.run(['git', 'push'])
+    # Add and commit with a unique message
+    subprocess.run(['git', 'add', filename], check=True)
+    subprocess.run(['git', 'commit', '-m', f"Adding letter '{letter}' on {commit_date.strftime('%Y-%m-%d')}"], env=env, check=True)
 
 # Commit each letter
 for i, letter in enumerate(letters):
-    create_commit(letter, commit_dates[i])
+    commit_date = start_date + timedelta(days=i)
+    create_commit(letter, commit_date)
 
-print("Completed committing the phrase 'BITCH PLZ'!")
+# Push once at the end
+subprocess.run(['git', 'push', '-u', 'origin', 'main'])
+
+print("✅ Completed committing the phrase 'BITCH PLZ'!")
